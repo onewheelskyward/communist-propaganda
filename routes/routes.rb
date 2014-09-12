76ca7ffabf9d71485061f15fbada6f4dd4212f65
Andrew Kreps <andrew.kreps@gmail.com>
2014-09-12 13:33:44 -0700

    Removing old route files.

--- routes/routes.rb
+++ routes/routes.rb
@@ -9,12 +9,17 @@ class App < Sinatra::Base
     uri.to_json
   end
 
-  get '/:propaganda' do
-    uri = resolve_bolshevik params[:propaganda]
-    redirect uri.bourgeois, 302
-  end
-
   get '/view' do
     erb :basic, :locals => {local_erb_var: 'xyz'}
+  end
+
+  # This one should remain at the end since it's a catch-all.
+  get '/:propaganda' do
+    shortening = resolve_bolshevik params[:propaganda]
+    if shortening
+      redirect shortening.bourgeois, 302
+    else
+      status 404
+    end
   end
 end
